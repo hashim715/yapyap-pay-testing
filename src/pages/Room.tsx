ce9f357888097655a5d2c1d464deb0c3f3166fc7
Muhammad Hashim <hashimmuhammad844@gmail.com>
2025-12-06 19:09:22 +0500

    added new changes

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -750,7 +750,6 @@ const Room = () => {
             setParticipants([]);
             setParticipantCount(0);
             setIsJoining(false);
-            socket.disconnect();
 
             if (timerRef.current) {
               clearInterval(timerRef.current);
